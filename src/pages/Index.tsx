cef7b334fb5f9ad6b70b572314ad19c8f70515b5
Haris Habib <harishabib.me@gmail.com>
2025-05-14 14:34:09 +0500

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,7 +26,7 @@ const Index = () => {
     date: "Until 15 April 2026"
  }, {
     title: "March - May 2025",
-    description: "The universities will get in touch with the nominated applicants directly to set a date and conduct the entrance exams from the middle of March until the end of May. Visit your chosen university’s individual page for complete details and to access PDFs of past entrance exams, if applicable.",
+    description: "Universities will contact nominated applicants directly to schedule entrance exams between mid-March and end of May. Check your chosen university’s page for full details and past exam PDFs, if available.",
     date: "May - July 2026"
  
   }, {
